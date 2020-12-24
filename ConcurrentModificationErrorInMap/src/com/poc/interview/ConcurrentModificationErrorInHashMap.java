974ac7cdfefb4eea1fba0fef71c07094005ecbf4
=SadanandDeo <=sada.edu88@gmail.com>
2020-12-24 17:19:46 +0530

    added code with output result

---
+++ ConcurrentModificationErrorInMap/src/com/poc/interview/ConcurrentModificationErrorInHashMap.java
@@ -0,0 +1,44 @@
+package com.poc.interview;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+
+public class ConcurrentModificationErrorInHashMap extends Thread{
+	static HashMap m=new HashMap();
+	
+	public  void run( )
+	{
+		try{
+			Thread.sleep(1000);
+			
+		}catch(InterruptedException ie)
+		{
+			
+		}
+		System.out.println("Child thread");
+		m.put(103,"C");
+	}
+
+	
+	public static void main(String... args ) throws InterruptedException
+	{
+		m.put(101,"A");
+		m.put(102,"B");
+		
+		ConcurrentModificationErrorInHashMap t = new ConcurrentModificationErrorInHashMap();
+		t.start();
+		Set keySet = m.keySet();
+		
+		Iterator itr = keySet.iterator();
+		
+		while(itr.hasNext())
+		{
+			Integer i=(Integer) itr.next();
+			System.out.println("Main thread Iterating Map and current entry is "+i+"   "+m.get(i));
+		Thread.sleep(3000);
+		}
+		System.out.println(m);
+	}
+
+}
